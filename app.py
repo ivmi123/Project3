30979d8bad890d0e774665ddddd12b545a2280c7
ivmi123 <ivmiandonova8b@gmail.com>
2026-01-15 12:40:28 +0200

    Implement selection saving and results display
    Added functionality to save user selections and display results.

--- app.py
+++ app.py
@@ -24,3 +24,28 @@ st.subheader("Избери любими неща")
 
 color = st.selectbox("Любим цвят:", list(st.session_state.colors.keys()))
 sport = st.selectbox("Любим спорт:", list(st.session_state.sports.keys()))
+color = st.selectbox("Любим цвят:", list(st.session_state.colors.keys()))
+sport = st.selectbox("Любим спорт:", list(st.session_state.sports.keys()))
+
+if st.button("Запази избора"):
+    st.session_state.colors[color] += 1
+    st.session_state.sports[sport] += 1
+    st.success("Изборът е записан!")
+
+st.divider()
+
+st.subheader("📈 Резултати")
+
+# Графика за цветовете
+st.write("Любими цветове")
+colors_df = pd.DataFrame.from_dict(
+    st.session_state.colors, orient="index", columns=["Брой"]
+)
+st.bar_chart(colors_df)
+
+# Графика за спортовете
+st.write("Любими спортове")
+sports_df = pd.DataFrame.from_dict(
+    st.session_state.sports, orient="index", columns=["Брой"]
+)
+st.bar_chart(sports_df)
