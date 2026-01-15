3e6d893dbd29448f685e3f14fee62f5b9ccb708f
ivmi123 <ivmiandonova8b@gmail.com>
2026-01-15 12:42:28 +0200

    Remove duplicate selectbox for color and sport
    Removed duplicate selectbox calls for favorite color and sport.

--- app.py
+++ app.py
@@ -24,8 +24,6 @@ st.subheader("Избери любими неща")
 
 color = st.selectbox("Любим цвят:", list(st.session_state.colors.keys()))
 sport = st.selectbox("Любим спорт:", list(st.session_state.sports.keys()))
-color = st.selectbox("Любим цвят:", list(st.session_state.colors.keys()))
-sport = st.selectbox("Любим спорт:", list(st.session_state.sports.keys()))
 
 if st.button("Запази избора"):
     st.session_state.colors[color] += 1
